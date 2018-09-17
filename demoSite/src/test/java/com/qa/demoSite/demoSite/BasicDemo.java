6860bd4c53cb16f032cef2b253e15c710692ce24
Lucy Hamilton <lwhamilton03@outlook.com>
2018-09-17 18:53:07 +0100

    modify

--- demoSite/src/test/java/com/qa/demoSite/demoSite/BasicDemo.java
+++ demoSite/src/test/java/com/qa/demoSite/demoSite/BasicDemo.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicDemo {
 
 	private WebDriver driver; 
-	private String str = "http://thedemosite.co.uk/";
-	
+	private String str = "http://thedemosite.co.uk/addauser.php";
+	private String url = "http://thedemosite.co.uk/login.php";
 	@Before 
 	public void setUp()
 	{
@@ -24,15 +24,63 @@ public class BasicDemo {
 	}
 	
 	@Test
-	public void basic()
+	public void basicUser()
 	{ 
 		driver.get(str);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		WebElement username = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input"));
-		username.click();
 		username.sendKeys("Kiwi");
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		WebElement password = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input"));
-		password.click();
-		password.sendKeys("IH8AutomatedTesting!"); 
+		password.sendKeys("IH8Testing");
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement sub = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input"));
+		sub.submit();
+		driver.get(str);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void basicLog()
+	{
+		driver.get(url);
+		WebElement log = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/p/input")); 
+		WebElement password = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/p/input")); 
+		WebElement testLogin = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input")); 
+		log.sendKeys("Kiwi");
+		password.sendKeys("IH8Testing");
+		testLogin.submit();
+		
+		driver.get(url);
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement status = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b"));
+		assertEquals("The login and password are invalid", "**No login attempted**", status.getText());
 	}
 	
 	@After
